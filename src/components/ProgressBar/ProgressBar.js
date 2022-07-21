225b11c6b9f24f84c4cea634df08c745c93c60b7
Julian Salischiker <julian.salischiker@valtech.com>
2022-07-21 16:59:52 -0300

    Finished progress bar and select.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,72 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    height: 8,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
+
+const Container = styled.span`
+  max-width: 370px;
+  width: 100%;
+
+  display: inline-block;
+  border-radius: var(--radius, 0px);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding, 0px);
+
+  /* Trim off corners */
+  overflow: hidden;
+`;
+
+const Filled = styled.span`
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+  display: block;
+  width: var(--width, 0%);
+  height: var(--height, 8px);
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error("Unknown size passed to ProgressBar");
+  }
+
+  return (
+    <Container
+      role="progressbar"
+      aria-valuenow={value}
+      style={{
+        "--padding": `${styles.padding || 0}px`,
+        "--radius": `${styles.radius}px`,
+      }}
+    >
+      <Filled
+        style={{
+          "--width": `${value}%`,
+          "--height": `${styles.height}px`,
+        }}
+      />
+    </Container>
+  );
 };
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalSelect = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size, 0);
+  height: var(--size, 0);
+  pointer-events: none;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  cursor: pointer;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalSelect>
+        {displayedValue}{" "}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={2} size={24} />
+        </IconWrapper>
+      </PresentationalSelect>
+    </Wrapper>
   );
 };
 
